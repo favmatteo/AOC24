82a72481c8b658f2080c6dca60d31591063b2666
favmatteo <matteo.favaro@outlook.com>
2024-12-01 23:30:06 +0100

    fix day 1

--- day1/part1.py
+++ day1/part1.py
@@ -1,21 +1,18 @@
 list1 = []
-dict2 = {}
+list2 = []
 with open("input.txt", "r") as f:
     for line in f.readlines():
         num1, num2 = line.strip().split()
-        num2 = int(num2)
         list1.append(int(num1))
-        if num2 in dict2:
-            dict2[int(num2)] += 1
-        else:
-            dict2[num2] = 1
+        list2.append(int(num2))
 
-print(dict2)
 
 res = 0
-for el in list1:
-    if el in dict2:
-        print(el, dict2[el])
-        res += el * dict2[el]
+while len(list1) > 0 and len(list2) > 0:
+    min1 = min(list1)
+    min2 = min(list2)
+    list1.remove(min1)
+    list2.remove(min2)
+    res += abs(min1 - min2)
 
 print(res)
--- day1/part2.py
+++ day1/part2.py
@@ -1,8 +1,21 @@
 list1 = []
-list2 = []
+dict2 = {}
 with open("input.txt", "r") as f:
     for line in f.readlines():
         num1, num2 = line.strip().split()
+        num2 = int(num2)
         list1.append(int(num1))
-        list2.append(int(num2))
+        if num2 in dict2:
+            dict2[int(num2)] += 1
+        else:
+            dict2[num2] = 1
 
+print(dict2)
+
+res = 0
+for el in list1:
+    if el in dict2:
+        print(el, dict2[el])
+        res += el * dict2[el]
+
+print(res)
